9dbee3a4e1de55099418b7eec9fe88f0ce399b10
Yuhan Ma <sarayhm@Yuhans-MBP.lan>
2020-09-17 23:18:49 -0400

    integrating sma and visualization

--- trading_algo_simu.py
+++ trading_algo_simu.py
@@ -1,7 +1,11 @@
 import pandas as pd
+from pandas import DataFrame
 from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
 import os
 from dotenv import load_dotenv
+import numpy as np
+import matplotlib.pyplot as plt
 # Importing hidden API Key.
 load_dotenv()
 api_key = os.getenv("API_KEY")
@@ -10,5 +14,27 @@ api_key = os.getenv("API_KEY")
 
 ts = TimeSeries(key = api_key, output_format = "pandas")
 data, meta_data = ts.get_intraday(symbol='MSFT',interval='60min', outputsize='full')
+data.sort_index(ascending = True, inplace = True)
+print(data)
 
-print(data.head(100))
+ti = TechIndicators(key = api_key, output_format = 'pandas')
+sma, sma_data = ti.get_sma(symbol='MSFT', interval='60min', time_period='60')
+sma.sort_index(ascending = True, inplace = True)
+print(sma)
+
+agg = data.join(sma, on = "date")
+agg.dropna(inplace = True)
+print(agg)
+
+
+ser1 = pd.Series(data['4. close'])
+ser2 = pd.Series(sma['SMA'])
+ser3 = pd.Series(data.reset_index()['date'])
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+
+ax1.plot(agg.index, agg["4. close"], c='b', label='msft')
+ax1.plot(agg.index, agg["SMA"], c='r', label='sma_msft')
+plt.legend(loc='upper left');
+plt.show()
